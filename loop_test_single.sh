ce288040f79e739061205b97ec7a237247540b46
Zakhar Akimov <azd.inform@gmail.com>
2023-07-13 15:28:55 +0300

    testing scripts improvements

--- loop_test_single.sh
+++ loop_test_single.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+fail_count=0
+color="0;32m"
+
 for ((i=1;i<=$1;i++))
 do
 	echo -e "\nIteration "$i"\n"
@@ -10,5 +13,9 @@ do
 		TTIME=$(date +%d_%H_%M)_$i
 		mkdir "broken_lists/$TTIME"
 		cp "list$2.txt" "broken_lists/$TTIME"
+		let "fail_count+=1"
+		color="0;31m"
 	fi
 done
+
+echo -e "\n$1 iterations on list$2:\033[$color $fail_count\033[0m failed"
