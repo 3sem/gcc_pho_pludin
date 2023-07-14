bc15631cac9d1e950edb3e88e046ee621077fa19
Zakhar Akimov <azd.inform@gmail.com>
2023-07-14 14:25:32 +0300

    benching fixes

--- loop_test_single.sh
+++ loop_test_single.sh
@@ -2,7 +2,12 @@
 
 fail_count=0
 color="0;32m"
-TTIME=$(date +%d_%H_%M)_$i
+TTIME=$(date +%d_%H_%M)
+list=$2
+if [ $2 = "2_break" ]
+then 
+	list=2
+fi
 
 for ((i=1;i<=$1;i++))
 do
@@ -12,11 +17,15 @@ do
 	make -C ./benches/$3 test_list$2
 	if [ $? -gt 0 ]
 	then
-		mkdir "broken_lists/$TTIME"
-		cp "list$2.txt" "broken_lists/$TTIME"
+		mkdir "broken_lists/${TTIME}_${i}"
+		cp "list$list.txt" "broken_lists/${TTIME}_${i}"
 		let "fail_count+=1"
 		color="0;31m"
 	fi
 done
 
 echo -e "\n$1 iterations on list$2:\033[$color $fail_count\033[0m failed"
+if [ $fail_count -gt 0 ]
+then 
+	echo "Run timestamp: $TTIME"
+fi
